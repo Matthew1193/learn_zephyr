27df019234b5197954e1c829f254a74234a6af88
matthew.oconnor <matthew.horneoconnor2@mail.dcu.ie>
2026-08-19 15:06:17 +0100

    fixed hyphen and underscore mix up

--- 02_external_led/src/main.c
+++ 02_external_led/src/main.c
@@ -1,7 +1,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
-#define STATUS_LED_NODE DT_ALIAS(my_ext_led)
+#define STATUS_LED_NODE DT_ALIAS(external_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(STATUS_LED_NODE, gpios);
 
