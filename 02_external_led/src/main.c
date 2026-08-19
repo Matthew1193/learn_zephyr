f3e9920d5fcc9b45f490651e0973bbeb5f107ddb
matthew.oconnor <matthew.horneoconnor2@mail.dcu.ie>
2026-08-19 14:50:27 +0100

    implemented custom device tree node and alias

---
+++ 02_external_led/src/main.c
@@ -0,0 +1,22 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define STATUS_LED_NODE DT_ALIAS(my_ext_led)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(STATUS_LED_NODE, gpios);
+
+int main(void)
+{
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
+
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+
+    while (1) {
+        gpio_pin_toggle_dt(&led);
+        k_msleep(500);
+    }
+
+    return 0;
+}
